fd73b1dc99cc791c033d275413953119b3599587
CoderGeek <shahajipatil22@gmail.com>
2021-05-05 16:11:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-die;
-echo "ahahaji"
+<?php
+include('Net/SSH2.php');
+
+$ssh = new Net_SSH2('www.domain.tld');
+$ssh->login('username', 'password');
+
+$ssh->read('[prompt]');
+$ssh->write("sudo command\n");
+$ssh->read('Password:');
+$ssh->write("Password\n");
+echo $ssh->read('[prompt]');
+?>
